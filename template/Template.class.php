9aec086811f2d5cd204bff42f7630870f54489bc
Joshua Rudd <joshuarudd@gmail.com>
2008-01-03 01:33:33 +0000

    streamline the function calls

--- template/Template.class.php
+++ template/Template.class.php
@@ -27,100 +27,9 @@ class Template
 		require_once LIB . 'database/Db.interface.php';
 		require_once LIB . 'database/AdaptorMysql.class.php';
 		$this->db = new AdaptorMysql();
-		
 		$this->siteConfig();
-		$this->requestUri = $this->requestUri();
-		$this->checkUrl();
 		$this->page();
 	}
-	
-	
-	
-	private function requestUri()
-	{
-		$uri = (($qsa = strpos($_SERVER['REQUEST_URI'],'?')) ? substr($_SERVER['REQUEST_URI'],0,$qsa) : $_SERVER['REQUEST_URI']);
-		$uri = explode("/",$uri);
-		$base_uri = explode("/",substr($_SERVER['PHP_SELF'],1,-(strlen('index.php') + 1)));
-		array_splice($uri,0,count($base_uri));
-		return $uri;
-	}
-	
-	
-	
-	private function checkUrl()
-	{
-		if ($this->error404 = ($this->page['id'] = $this->pageId()) ? false : true) {
-			$this->page['name'] = 'Error 404';
-			header('HTTP/1.0 404 Not Found');
-		}
-	}
-	
-	
-	
-	private function pageId($parentId=0,$level=0,$id=0)
-	{
-		if ($this->requestUri[0] == '') {
-			return -1;
-		} else {
-			if (isset($this->requestUri[$level])) {
-				if ($q = $this->db->queryRow("
-					SELECT id,name,slug,parent_id
-					FROM site_map
-					WHERE active = '1'
-						AND parent_id = '".$parentId."'
-						AND slug IN ('".$this->requestUri[$level]."','*')
-				")) {
-					if ($q['slug'] == '*') return $q['id'];
-					else return $this->pageId($q['id'],$level+1,$q['id']);;
-				}
-			} else {
-				return $id;
-			}
-		}
-	}
-	
-	
-	
-	/*
-	
-	Function: tpl
-	
-	Figures out which TPL file to use for a given page request
-	
-	Parameters:
-	
-		tpl_path:String - optional path to the TPL file directory
-	
-	Returns:
-	
-		string
-	
-	*/
-	
-	private function page()
-	{
-		$tpl_path = defined('TPL') ? TPL : 'php/tpl';
-		if ($this->page['id'] < 0 || ($this->page['id'] && $this->page = $this->db->queryRow("
-			SELECT *
-			FROM ".TABLE_SITE_MAP."
-			WHERE active = '1'
-				AND id = '".$this->page['id']."'
-		"))) {
-			// Determine which TPL file to use
-			if ($this->page['id'] < 0 || !$this->page['tpl']) $this->page['tpl'] = $this->requestUri[0] ? implode('.',$this->requestUri) : 'home';
-			// If actual TPL file doesn't exist, use a generic one
-			$this->page['tpl_file'] = 'hello world';
-			if (file_exists($_SERVER['DOCUMENT_ROOT'].'/'.$tpl_path.'/'.$this->page['tpl'].'.php')) {
-				$this->page['tpl_file'] = $_SERVER['DOCUMENT_ROOT'].'/'.$tpl_path.'/'.$this->page['tpl'].'.php';
-			} else {
-				$this->page['tpl_file'] = $_SERVER['DOCUMENT_ROOT'].'/'.$tpl_path.'/_generic.php';
-			}
-		} else {
-			print 'nope';
-		}
-	}
-	
-	
 	
 	/*
 	
@@ -151,7 +60,106 @@ class Template
 		}
 	}
 	
+	/*
 	
+	Function: page
+	
+	Load page into memory
+	
+	*/
+	
+	private function page()
+	{
+		// Get URI request and put it into an array
+		$this->requestUri = $this->requestUri();
+		// Check URL to make sure it exists
+		if ($this->error404 = ($this->page['id'] = $this->pageId()) ? false : true) {
+			$this->page['name'] = 'Error 404';
+			header('HTTP/1.0 404 Not Found');
+		}
+		// Set TPL path
+		$tpl_path = defined('TPL') ? TPL : 'php/tpl';
+		// Load page variables
+		if ($this->page['id'] < 0 || ($this->page['id'] && $this->page = $this->db->queryRow("
+			SELECT *
+			FROM ".TABLE_SITE_MAP."
+			WHERE active = '1'
+				AND id = '".$this->page['id']."'
+		"))) {
+			// Determine which TPL file to use
+			if ($this->page['id'] < 0 || !$this->page['tpl']) $this->page['tpl'] = $this->requestUri[0] ? implode('.',$this->requestUri) : 'home';
+			// If actual TPL file doesn't exist, use a generic one
+			if (file_exists($_SERVER['DOCUMENT_ROOT'].'/'.$tpl_path.'/'.$this->page['tpl'].'.php')) {
+				$this->page['tpl_file'] = $_SERVER['DOCUMENT_ROOT'].'/'.$tpl_path.'/'.$this->page['tpl'].'.php';
+			} else {
+				$this->page['tpl_file'] = $_SERVER['DOCUMENT_ROOT'].'/'.$tpl_path.'/_generic.php';
+			}
+		} else {
+			// If the page doesn't exist, use the Error 404 tpl file
+			$this->page['tpl_file'] = $_SERVER['DOCUMENT_ROOT'].'/'.$tpl_path.'/_error404.php';
+		}
+	}
+	
+	/*
+	
+	Function: requestUri
+	
+	Strip any queries from the request URI and return an array
+	
+	Returns:
+	
+		array
+	
+	*/
+	
+	private function requestUri()
+	{
+		$uri = (($qsa = strpos($_SERVER['REQUEST_URI'],'?')) ? substr($_SERVER['REQUEST_URI'],0,$qsa) : $_SERVER['REQUEST_URI']);
+		$uri = explode("/",$uri);
+		$base_uri = explode("/",substr($_SERVER['PHP_SELF'],1,-(strlen('index.php') + 1)));
+		array_splice($uri,0,count($base_uri));
+		return $uri;
+	}
+	
+	/*
+	
+	Function: pageId
+	
+	Determine the DB page id for the requested URL
+	
+	Parameters:
+	
+		parentId:Integer - Parent Id to start from
+		level:Integer - how deep the request is
+		id:Integer - the current page id
+	
+	Returns:
+	
+		integer
+	
+	*/
+	
+	private function pageId($parentId=0,$level=0,$id=0)
+	{
+		if ($this->requestUri[0] == '') {
+			return -1;
+		} else {
+			if (isset($this->requestUri[$level])) {
+				if ($q = $this->db->queryRow("
+					SELECT id,name,slug,parent_id
+					FROM site_map
+					WHERE active = '1'
+						AND parent_id = '".$parentId."'
+						AND slug IN ('".$this->requestUri[$level]."','*')
+				")) {
+					if ($q['slug'] == '*') return $q['id'];
+					else return $this->pageId($q['id'],$level+1,$q['id']);;
+				}
+			} else {
+				return $id;
+			}
+		}
+	}
 	
 	/*
 	
