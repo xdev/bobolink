cf509cf7171773115ad03500d743f020b2867ff3
Charles Mastin <charlesmastin@gmail.com>
2008-03-26 18:03:34 +0000

    Updated Google Analytics markup (new version)

--- template/Template.class.php
+++ template/Template.class.php
@@ -293,14 +293,20 @@ class Template
 	
 	public function googleAnalytics()
 	{
+		
+		
 		if (defined('GOOGLE_ANALYTICS')) {
 			return sprintf(
 				'
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
-	<script type="text/javascript">
-	_uacct = "%s";
-	urchinTracker();
-	</script>',
+				<script type="text/javascript">
+					var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+					document.write(unescape("%%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%%3E%%3C/script%%3E"));
+				</script>
+				<script type="text/javascript">
+					var pageTracker = _gat._getTracker("%s");
+					pageTracker._initData();
+					pageTracker._trackPageview();
+				</script>',
 				GOOGLE_ANALYTICS
 			);
 		}
